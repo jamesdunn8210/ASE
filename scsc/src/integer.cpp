8a473648ec01e9fe71176070bc489f2af2746cd3
james <jamesdunn8210@gmail.com>
2018-05-10 16:49:43 +0100

    < & == operators added

--- scsc/src/integer.cpp
+++ scsc/src/integer.cpp
@@ -9,8 +9,55 @@ namespace ExactArithmetic
 	{
 		digits.push_back(0);
 	}
-	Integer::Integer(unsigned long long int)
+
+	Integer::Integer(unsigned long long int d)
+	{
+		digits.push_back(d);
+	}
+
+	bool Integer::operator<(const Integer & rhs) const
 	{
 		
+		if (this->digits.size() < rhs.digits.size()) //lhs smaller length
+		{ 
+			return true;
+		}
+		
+
+		auto lhs_it = this->digits.begin();
+		auto rhs_it = rhs.digits.begin();
+
+		for (int i = 0; i <= this->digits.size(); i++) {
+
+			if (*lhs_it < *rhs_it)
+			{
+				return true;
+			}
+			else if (*lhs_it > *rhs_it)
+			{
+				return false;
+			}
+
+		}
+
+		return false;
+	}
+
+	bool Integer::operator==(const Integer & rhs) const
+	{
+
+		if (this->digits == rhs.digits) {
+			return true;
+		}
+		return false;
+		
+	}
+	bool Integer::operator!=(const Integer & rhs) const
+	{
+		if( !(this->operator==(rhs)) )
+		{
+			return true;
+		}
+		return false;
 	}
 }
--- scsc/src/rational-interactive.cpp
+++ scsc/src/rational-interactive.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "../headers/rational.h"
+#include "../headers/integer.h"
 
 using namespace ExactArithmetic;
 
@@ -11,41 +12,50 @@ using std::endl;
 int main()
 {
 
-    cout << "Enter the left operand (e.g. 1/3): ";
-    Rational x;
-    cin >> x;
+	Integer a(9);
+	Integer b(15);
 
-    cout << "Enter the operator (+, -, * or /): ";
-    std::string op;
-    cin >> op;
+	int c = a.operator<(b);
+		
+	std::cout << c;
 
-    cout << "Enter the right operand (e.g. 4/9): ";
-    Rational y;
-    cin >> y;
 
-    Rational result;
-    if (op == "+")
-    {
-        result = x + y;
-    }
-    else if (op == "-")
-    {
-        result = x - y;
-    }
-    else if (op == "*")
-    {
-        result = x * y;
-    }
-    else if (op == "/")
-    {
-        result = x / y;
-    }
-    else
-    {
-        cout << "Unrecognised operator" << endl;
-        return 1;
-    }
-    cout << result << endl;
+
+    //cout << "Enter the left operand (e.g. 1/3): ";
+    //Rational x;
+    //cin >> x;
+
+    //cout << "Enter the operator (+, -, * or /): ";
+    //std::string op;
+    //cin >> op;
+
+    //cout << "Enter the right operand (e.g. 4/9): ";
+    //Rational y;
+    //cin >> y;
+
+    //Rational result;
+    //if (op == "+")
+    //{
+    //    result = x + y;
+    //}
+    //else if (op == "-")
+    //{
+    //    result = x - y;
+    //}
+    //else if (op == "*")
+    //{
+    //    result = x * y;
+    //}
+    //else if (op == "/")
+    //{
+    //    result = x / y;
+    //}
+    //else
+    //{
+    //    cout << "Unrecognised operator" << endl;
+    //    return 1;
+    //}
+    //cout << result << endl;
 
     return 0;
 }
